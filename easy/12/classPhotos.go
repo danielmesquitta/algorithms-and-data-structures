3769f667f1fd35510cc75145a810bd9ba06ddb71
Daniel Mesquita <danielmesquitta123@gmail.com>
2024-02-29 15:32:26 -0300

    feat: add ClassPhotos

---
+++ easy/12/classPhotos.go
@@ -0,0 +1,26 @@
+package easy12
+
+import "sort"
+
+func ClassPhotos(redShirtHeights []int, blueShirtHeights []int) bool {
+	blueIsTaller := true
+	redIsTaller := true
+
+	sort.Ints(redShirtHeights)
+	sort.Ints(blueShirtHeights)
+
+	for i, red := range redShirtHeights {
+		blue := blueShirtHeights[i]
+		if blue >= red {
+			redIsTaller = false
+		}
+		if red >= blue {
+			blueIsTaller = false
+		}
+		if !redIsTaller && !blueIsTaller {
+			break
+		}
+	}
+
+	return blueIsTaller || redIsTaller
+}
